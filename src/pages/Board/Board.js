d98f40289eda947e83a07fc5edbcf4d73f8213ff
Melory Ayala <melory.ayala@gmail.com>
2023-09-21 18:57:41 +0200

    Creating board and displaying tasks

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,52 @@
+import Lane from '../../components/Lane/Lane';
+import './Board.css';
+import {useEffect, useState} from "react";
+
+const lanes = [
+    {id: 1, title: 'To Do'},
+    {id: 2, title: 'In Progress'},
+    {id: 3, title: 'Review'},
+    {id: 4, title: 'Done'},
+];
+
+function Board() {
+    const [loading, setLoading] = useState(true);
+    const [tasks, setTasks] = useState([])
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const tasks = await fetch('https://my-json-server.typicode.com/PacktPublishing/React-Projects-second-Edition/tasks');
+                const result = await tasks.json();
+
+                if (result) {
+                    setTasks(result)
+                    setLoading(false)
+                }
+
+            } catch (e) {
+                setLoading(false);
+                setError(e.message)
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    return (
+        <div className='Board-wrapper'>
+            {lanes.map((lane) => (
+                <Lane
+                    key={lane.id}
+                    title={lane.title}
+                    loading={loading}
+                    error={error}
+                    tasks={tasks.filter(task => task.lane === lane.id)}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Board;
